344b95e817559ac75508d0159055906b17246880
Hoarau Matthias <h.matthias@live.fr>
2022-07-19 09:01:29 +0400

    add component in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
+import HomeHero from "./components/HomeHero/HomeHero";
+import Skill from "./components/Skill/Skill";
+import Project from "./components/Project/Project";
+import Contact from "./components/Contact/Contact";
 import './App.css'
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <HomeHero />
+        <Skill />
+        <Project />
+        <Contact />
     </div>
   );
 }
